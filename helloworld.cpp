9c22dbeb79ab9237bf602aa5371abcf158ec80e3
Jolusca <jlucasoliveira2002@gmail.com>
2026-05-12 17:44:01 -0300

    dia1 Curso

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(void){
+    cout << "its me, your first program";
+    cout << endl;
+
+    cout << "its me, your second program\n";
+
+    return 0;
+}
